7145f28c9a98ac76c896bbb738e99e14d3a197f2
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-09 02:58:14 -0800

    intergrating a loop

--- number.py
+++ number.py
@@ -1,7 +1,8 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x is not an integer")
-# Use of else statementto solve the NameError
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not an integer")
+    # Use of else statementto solve the NameError
+    else:
+        print(f"x is {x}")
