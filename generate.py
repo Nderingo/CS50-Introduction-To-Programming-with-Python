ee96bd3e9825297db68ae17fc4f1fc78be5458bb
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-10 03:33:40 -0800

    function random.shuffle

--- generate.py
+++ generate.py
@@ -4,5 +4,10 @@ import random
 #coin = random.choice(["heads", "tails"])
 #print(coin)
 
-number = random.randint(1,12)
-print(number)
+#number = random.randint(1,12)
+#print(number)
+
+cards = ["queen", "king","jack","joker"]
+random.shuffle(cards)
+for cards in cards:
+    print(cards)
