4523e3aec380f7761b11519180cdf31d9d9a8de8
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-11 22:29:39 -0800

    use of sys.exit to bypass the IndexError when the list isout of reach

--- name.py
+++ name.py
@@ -1,7 +1,10 @@
 import sys
+
+#Check for Errors
 if len(sys.argv) < 2:
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
+    sys.exit("Too many arguments")
+
+# Print name tags
+print("Hello, my name is", sys.argv[1])
