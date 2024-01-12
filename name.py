85f44fcef8a1432b51f171f77be625b375565e42
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-11 22:39:44 -0800

    using the slice method when having a list to skip position 0 on the list

--- name.py
+++ name.py
@@ -3,8 +3,12 @@ import sys
 #Check for Errors
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments")
+#elif len(sys.argv) > 2:
+#    sys.exit("Too many arguments")
+
+# For multiple arguments
+for arg in sys.argv[1:]:
 
 # Print name tags
-print("Hello, my name is", sys.argv[1])
+#    print("Hello, my name is", sys.argv[1])
+    print("Hello, my name is", arg)
