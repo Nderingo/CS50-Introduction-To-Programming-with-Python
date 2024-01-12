9ed47011f4a978be85d9cb5bb605e138714ba787
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-11 22:42:26 -0800

    slicing from the end of the argument vetor

--- name.py
+++ name.py
@@ -7,7 +7,7 @@ if len(sys.argv) < 2:
 #    sys.exit("Too many arguments")
 
 # For multiple arguments
-for arg in sys.argv[1:]:
+for arg in sys.argv[1:-2]:
 
 # Print name tags
 #    print("Hello, my name is", sys.argv[1])
