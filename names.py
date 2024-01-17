5003f69d905d7f002dab8b8594f2f2d3ec59331c
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-17 03:43:39 -0800

    append method and sort

--- names.py
+++ names.py
@@ -1,2 +1,6 @@
-name = input("What is your name? ")
-print(f"hello, {name}")
+names = []
+
+for _ in range(3):
+    names.append(input("What is your name? "))
+for name in sorted(names):
+    print(f"hello, {name}")
