2bfe39946e8ef4ce5b8ab7f9e505cd7c9b2d74da
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-17 22:49:28 -0800

    using open function in file I/O

--- names.py
+++ names.py
@@ -1,7 +1,11 @@
-names = []
+#names = []
 
-for _ in range(3):
-    names.append(input("What is your name? "))
+#for _ in range(3):
+#    names.append(input("What is your name? "))
     
-for name in sorted(names):
-    print(f"hello, {name}")
+#for name in sorted(names):
+#    print(f"hello, {name}")
+
+name = input("What is your name? ")
+
+open("names.txt", "w")
