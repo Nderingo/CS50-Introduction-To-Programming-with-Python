0b290533000dbc4bfd279ae8dab4d7cc55cd34e5
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-19 22:43:40 -0800

    updated students.py file

--- students.py
+++ students.py
@@ -11,6 +11,6 @@ with open("students.csv") as file:
     for line in file:
         names,house,years = line.rstrip().split(",")
         students.append(f"{names} is in {house} for {years} years")
-
-for student in sorted(students):
-    print(student)
+#Sortings
+#for student in sorted(students):
+#    print(student)
