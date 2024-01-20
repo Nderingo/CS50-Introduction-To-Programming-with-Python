b081cca17b0058257caf5f7ec2ec655faad02b24
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-19 22:33:14 -0800

    cleaning up the code to make it more readable

--- students.py
+++ students.py
@@ -2,3 +2,12 @@ with open("students.csv") as file:
     for line in file:
         name, house, years = line.rstrip().split(",")
         print(f"{name} is in {house} for {years}")
+
+#Alternatively
+        
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        names,house,years = line.rstrip().split(",")
+        students.append(f"{names} is in {house} for {years}")
