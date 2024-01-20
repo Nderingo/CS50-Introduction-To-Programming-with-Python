74cf5a8242aa574af54301fcee0a41a2d30f0aeb
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-19 22:51:43 -0800

    updated students.py file by creating a dictionary

--- students.py
+++ students.py
@@ -9,8 +9,14 @@ students = []
 
 with open("students.csv") as file:
     for line in file:
-        names,house,years = line.rstrip().split(",")
-        students.append(f"{names} is in {house} for {years} years")
+        name,house,years = line.rstrip().split(",")
+        students.append(f"{name} is in {house} for {years} years")
 #Sortings
 #for student in sorted(students):
 #    print(student)
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        student["years"] = years
+        students.append(student)
+        
