39af81f6ebe60af9486654499115e3bc644fe5a9
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-19 22:38:27 -0800

    sorted out students list in ascending order

--- students.py
+++ students.py
@@ -11,3 +11,6 @@ with open("students.csv") as file:
     for line in file:
         names,house,years = line.rstrip().split(",")
         students.append(f"{names} is in {house} for {years}")
+
+for student in sorted(students):
+    print(student)
