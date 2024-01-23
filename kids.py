3c152615b3b90642fb3b1ad548cc46fe5c1d600a
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-23 01:23:58 -0800

    wrote to the csv file through prompting user input

--- kids.py
+++ kids.py
@@ -3,3 +3,7 @@ import csv
 name = input("What is your name? ")
 home = input("Where is your home? ")
 
+with open("kids.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([home, name])
+    
