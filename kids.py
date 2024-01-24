be5b5dc268935bc86fd47e7a7bd99fda212c2291
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-24 01:07:07 -0800

    using DickWriter to write csv file

--- kids.py
+++ kids.py
@@ -3,6 +3,12 @@ import csv
 name = input("What is your name? ")
 home = input("Where is your home? ")
 
+#with open("kids.csv", "a") as file:
+ #   writer = csv.writer(file)
+#    writer.writerow([name, home])
+
+#Alternatively
 with open("kids.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name, home])
+    writer = csv.DictWriter(file,fieldnames=["name", "home"])
+    writer.writerow({"name":name, "home":home})
+
