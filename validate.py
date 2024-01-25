2893c2c8f11f16be9e1a8fe4400ab465d08852cd
Emmanuel Lyimo <esal29t@gmail.com>
2024-01-24 21:49:00 -0800

    using re library in regular expressions

--- validate.py
+++ validate.py
@@ -1,8 +1,15 @@
+import re
+
 email = input("What is your email address? ").strip()
 
-username, domain = email.split("@")
+#username, domain = email.split("@")
 
-if username and domain.endswith(".edu"):
+#if username and domain.endswith(".edu"):
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+if re.search(".+@.+", email):
     print("Valid")
 else:
     print("Invalid")
